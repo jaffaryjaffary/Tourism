cf43a3c3819da3bec0dd3edb16a51303e815813e
Jaffary Jaffery <jaffaryjaffery9@gmail.com>
2026-04-08 01:22:56 +0300

    fix contact page error

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,7 +1,7 @@
 import NavBar from "@/components/NavBar";
 import Contact from "../../components/Get_contant";
 import { FetchAllDestinationdAction } from "../Actions";
-export default async function Contact(){
+export default async function ContactPage(){
  
     const FetchDestination = await FetchAllDestinationdAction()
 
