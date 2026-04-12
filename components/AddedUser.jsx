68874971501c7608a2de8cd8276284956573293a
Jaffary Jaffery <jaffaryjaffery9@gmail.com>
2026-04-12 11:52:49 +0300

    fix contact page error

--- components/AddedUser.jsx
+++ components/AddedUser.jsx
@@ -1,48 +1,12 @@
 'use client'
- import { useEffect, useState } from "react"
-
-import { IoMdCheckmark } from "react-icons/io";
 import { registerAction } from "../app/Actions";
-
-
- export default function AddedUserPage(){
-
-
-         const [success, setSuccess] = useState(false)
-   const HandleSubmit=()=>{
-
-          if(success){
-            // setSuccess(false)
-              setSuccess(true)
-          }   
-        
-   }
-  
-
-    
-   
-
-    
-        
-    
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            setSuccess(false)
-        },40000)
-    })  
-    
- 
+ export default function AddedUserPage(){ 
     return(
         <div className="p-4 mt-3">
-
-          
-
-        
              
             <div>
                
-                <h1 className="text-2xl font-semibold">Fill Your Profile Information</h1>
+                <h1 className="text-2xl font-semibold">Fill  Profile Information</h1>
                  <form action={registerAction}   className="mt-6">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
                     
@@ -51,10 +15,6 @@ import { registerAction } from "../app/Actions";
                         <input type="text" name="fname" placeholder="Enter FirstName"  className="mt-1 block w-full 
                     rounded-md
                      border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-4"
-                        //  value={formData.fname}
-                        //  onChange={(event)=>setFormData({...formData,fname:event.target.value})}
-
-                        
                         required
                         />
                         
@@ -66,9 +26,6 @@ import { registerAction } from "../app/Actions";
                         <input type="text" name="lname" placeholder="Enter LastName"  className="mt-1 block w-full 
                     rounded-md
                      border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-4"
-                        //    value={formData.lname}
-                        //  onChange={(event)=>setFormData({...formData,lname:event.target.value})}
-                       
                         required
                         />
                        
@@ -104,15 +61,11 @@ import { registerAction } from "../app/Actions";
                         >
                         <option value="">Select Role</option>
                              <option value="Admin">Admin</option>
-                           <option value="Normal User">Normal User</option>
-                        
-                           
+                           <option value="Normal User">Normal User</option> 
                         
                         </select>
                          
                     </div>
-
-                   
 
                      <div className="flex flex-col">
                          <label htmlFor="">Select Gender</label>
@@ -130,44 +83,15 @@ import { registerAction } from "../app/Actions";
                     </div>
 
                      <div className="flex flex-col">
-                        <button type="submit"  onClick={HandleSubmit}
+                        <button type="submit" 
                     className="text-xl font-bold p-3 bg-black text-white cursor-pointer  mt-2 hover:opacity-60">OnBoard as System User</button>
                     </div>
-                       
-                    
-                    
-
-                
-
-            
-              {success &&(
-                <div className="flex items-center justify-center">
-                         <div className="bg-green-200 shadow-lg mt-0 p-4 border-b-4 border-green-400">
-                 {success &&(<span className="flex gap-3 items-center">
-                    <IoMdCheckmark size={24} color="green" /><p>Congratulation! Information added successfully</p></span>)}
-                
-             </div>
-                      </div>
-            
-           
-              )}
             
                 </div>
 
                
                 </form>
             </div>
-
-                 
-            
-                {/* <div className="flex items-center justify-center mt-10">
-                    <div className="flex flex-col">
-                        <h1 className="text-2xl font-bold text-center">Please you do not have permission to access this page</h1>
-                          <span className="flex items-center justify-center mt-5"><IoShield  size={130}/></span>
-                          </div>
-                    </div> */}
-                     
-                 
                
         </div>
     )
