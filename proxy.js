3cb9b3317c9b299c0c8a4d4026139a10ae5bb522
Jaffary Jaffery <jaffaryjaffery9@gmail.com>
2026-06-20 12:56:06 +0300

    add full project (client, server, database)

--- proxy.js
+++ proxy.js
@@ -55,19 +55,55 @@
 
 
 
-export function proxy(request) {
-    const token = request.cookies.get("token");
+// export function proxy(request) {
+//     const token = request.cookies.get("token");
 
-    if (!token) {
-        return NextResponse.redirect(new URL("/Login", request.url));
+//     if (!token) {
+//         return NextResponse.redirect(new URL("/Login", request.url));
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/12345UploadImg", "/Admin_Dashboard","/12345VdstId", "/1234567","/123456789", "/Add_Visitor","/Approved_Visitor",
+//         "/Contact_Visitor", "/Help&Support", "/Upload_success","/ViewUserDetails", "/User_Table"
+//     ],
+    
+// };
+
+
+
+
+export function proxy(req) {
+    const path = req.nextUrl.pathname;
+
+    const publicPaths = ['/', '/Login'];
+    const isPublicPath = publicPaths.includes(path);
+
+    const token = req.cookies.get("token")?.value || '';
+
+    // User already logged in
+    if (isPublicPath && token) {
+        return NextResponse.redirect(
+            new URL('/Admin_Dashboard', req.url)
+        );
+    }
+
+    // User not logged in
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(
+            new URL('/Sign-in', req.url)
+        );
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/12345UploadImg", "/Admin_Dashboard","/12345VdstId", "/1234567","/123456789", "/Add_Visitor","/Approved_Visitor",
-        "/Contact_Visitor", "/Help&Support", "/Upload_success","/ViewUserDetails", "/User_Table"
+    matcher: [
+        '/',
+        '/Login',
+        '/Admin_Dashboard/:path*',
     ],
-    
 };
